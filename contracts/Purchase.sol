1df2421ada9667ea9d0f5a5a43133f56045a8e9c
Riccardo Cavallin <riccardocavallin14@gmail.com>
2020-09-10 14:42:08 +0200

    limit setted to 100

--- contracts/Purchase.sol
+++ contracts/Purchase.sol
@@ -6,7 +6,7 @@ contract Purchase {
 
     // Buying a data
     function buy(uint dataId) public returns (uint) {
-        require(dataId >= 0 && dataId <= 15);
+        require(dataId >= 0 && dataId <= 100);
         buyers[dataId] = msg.sender;
         return dataId;
     }
